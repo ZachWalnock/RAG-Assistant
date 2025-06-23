0f4f3d9becbeecbd6ed8112eb6cc0356115e8e7d
zach <zdw5098@psu.edu>
2025-06-22 21:23:18 -0400

    app creation and exploration

---
+++ app/components/Chatbox.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Chatbox = () => {
+  return (
+    <div className='w-full flex justify-center items-center'>
+        Hello!
+    </div>
+  )
+}
+
+export default Chatbox;
